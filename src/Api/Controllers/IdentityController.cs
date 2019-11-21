0bb147b2e8baedf7e891ffef2eb89213c1f9b2d3
Raul Lorca <raul.lorca@softmachine.es>
2019-11-21 15:43:30 +0100

    Proteccion de una API mediante credenciales de cliente

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+[Route("identity")]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+    }
+}
